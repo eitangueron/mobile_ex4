cb7cea4091358051939eed812fb73bec66d0bec3
Eitan Gueron <eitangueron@gmail.com>
2021-11-30 11:00:04 +0200

    missing remove and elegent json work

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs');
+// const bodyParser = require('body-parser'); 
+// app.use(bodyParser.json());
+
+// tasks structre  [ { id: int , task :  string }, ... ]
+const tasks = JSON.parse ( fs.readFileSync('./tasks.json', 'utf8') )
+// console.log(tasks)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Eitan\'s server!')
+    res.send('Welcome to Eitan\'s server!')
 })
 
-app.get('/tasks', (req,res) => {
-    res.send()
+app.get('/tasks', (req, res) => {
+    res.send(tasks)
 })
 
-app.post('/tasks/new', (req,res) => {
-    // add to json
-    res.send()
+// example: /tasks/new?id=1&taks=do somthing
+app.get('/tasks/new/:id/:task', (req, res) => {
+
+    let newTask = {...req.params}
+
+    // Try elegntly append and not completly rewrite?
+    tasks.allTasks.push(newTask)
+    
+    fs.writeFile('./tasks.json', JSON.stringify(tasks), (err)=> {
+        err ? res.send(err) : res.send(`Added task ${newTask.id} successfully`) 
+    })
 })
 
-app.delete('tasks/remove', (req,res) => {
-    // delete user by id
-    res.send()
+// example: /tasks/remove?id=1
+app.get('tasks/remove/:id', (req, res) => {
+    let idToDelete = req.params.id
+
+    tasks.allTasks.remove(idToDelete)
+    
+    res.send('done')
+    fs.writeFile('./tasks.json', JSON.stringify(tasks), (err)=> {
+        err ? res.send(err) : res.send(`Removed task ${idToDelete} successfully`) 
+    })
 })
 
 app.listen(port, () => {
-  console.log(`server up and running on port ${port}`)
+    console.log(`server up and running on port ${port}`)
 })
